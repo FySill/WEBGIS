adfa4633f4f599bd01c243aee6c000f4054e2b2b
Cheng Fei <971841271@qq.com>
2014-10-12 10:39:49 +0800

    add new algorithm

--- src/main/java/fy/gisweb/action/BasePointsAction.java
+++ src/main/java/fy/gisweb/action/BasePointsAction.java
@@ -11,6 +11,12 @@ import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import fy.gisweb.model.Extend;
+import fy.gisweb.model.ModelPoints;
+import fy.gisweb.model.Points;
+import fy.gisweb.model.Rings;
+import fy.gisweb.service.impl.ModelPointsService;
+
 public class BasePointsAction extends ActionSupport {
 	private HttpServletRequest request;
 	private HttpServletResponse response;
@@ -20,9 +26,8 @@ public class BasePointsAction extends ActionSupport {
 	public String BasePoints() throws IOException {
 		request = ServletActionContext.getRequest();
 		String dataSource = request.getParameter("data");
+		//string2double
 		String dataNum = dataSource.substring(2, dataSource.length()-2);
-		
-//		request.getAttribute("color");
 		String[] strs = dataNum.split("\\,");
 		List<Double> nums = new ArrayList<Double>();
 		for(String string:strs){
@@ -30,12 +35,41 @@ public class BasePointsAction extends ActionSupport {
 			double num=Double.parseDouble(string);
 			nums.add(num);
 		}
-		for(Double n:nums){
-			System.out.println(n);
+		//double2XY & generateExtend
+		Points bps = new Rings(); 
+		Extend exd = new Extend();
+		ArrayList<Rings> rings = new ArrayList<Rings>();
+		for(int i = 0;i < nums.size()-2;i=i+2){
+			double x = nums.get(i);
+			double y = nums.get(i+1);
+			if(i == 0){
+				exd.setBottom(y);
+				exd.setTop(y);
+				exd.setLeft(x);
+				exd.setRight(x);
+			}else{
+				if(exd.getLeft()>x){
+					exd.setLeft(x);
+				}
+				if(exd.getRight()<x){
+					exd.setRight(x);
+				}
+				if(exd.getBottom()>y){
+					exd.setBottom(y);
+				}
+				if(exd.getTop()<y){
+					exd.setTop(y);
+				}
+			}
+			bps.setX(x);
+			bps.setY(y);
+			rings.add((Rings)bps);
+			
 		}
-		// Array[] rings = requests
-		
-		// List<BasePoints> data = basePointsService.generateBasePoints(rings);
+		//generateBasePoints
+		ModelPointsService<ModelPoints> mps = new ModelPointsService<ModelPoints>();
+		double gridSize = 5;//define grid size
+		List<ModelPoints> data = mps.generateModelPoints(rings,gridSize);
 		return SUCCESS;
 	}
 
